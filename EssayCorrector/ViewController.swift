af62669548687f737a825c8842b02221aa9064e9
Jose Rafael Leon Bigio Anton <sirownsalot@gmail.com>
2016-04-18 21:53:33 -0500

    Initial Commit

---
+++ EssayCorrector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EssayCorrector
+//
+//  Created by Jose Bigio on 4/18/16.
+//  Copyright © 2016 Jose Bigio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
